a9da0b6906bece7c760447ceb4d9ac6269f3372f
Chris Kent <truenorthcreative@gmail.com>
2016-05-19 20:25:34 -0700

    Update ConfigureRemotingForAnsible.ps1

--- scripts/ConfigureRemotingForAnsible.ps1
+++ scripts/ConfigureRemotingForAnsible.ps1
@@ -101,14 +101,8 @@ ElseIf ((Get-Service "WinRM").Status -ne "Running")
 # WinRM should be running; check that we have a PS session config.
 If (!(Get-PSSessionConfiguration -Verbose:$false) -or (!(Get-ChildItem WSMan:\localhost\Listener)))
 {
-  if ($SkipNetworkProfileCheck) {
     Write-Verbose "Enabling PS Remoting without checking Network profile."
     Enable-PSRemoting -SkipNetworkProfileCheck -Force -ErrorAction Stop
-  }
-  else {
-    Write-Verbose "Enabling PS Remoting"
-    Enable-PSRemoting -Force -ErrorAction Stop
-  }
 }
 Else
 {
